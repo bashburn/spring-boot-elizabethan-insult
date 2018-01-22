3773508265e83ac7cf7c5641670dd47a8c2fb67a
Brian Ashburn <ashburn.brian@gmail.com>
2018-01-22 17:10:53 -0500

    Fix some fat finger error in a file

--- insult-service/src/main/java/com/bashburn/demo/insult/InsultApplication.java
+++ insult-service/src/main/java/com/bashburn/demo/insult/InsultApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
- had @EnableCircuitBreaker
+@EnableCircuitBreaker
 public class InsultApplication {
   public static void main(String[] args) {
     SpringApplication.run(InsultApplication.class, args);
